cb3377293eb614a9d62f538789a837eebeca2c89
SemenInc <52431073+SemenInc@users.noreply.github.com>
2024-09-11 23:09:32 +0200

    Update scanSecrets.groovy

--- vars/scanSecrets.groovy
+++ vars/scanSecrets.groovy
@@ -1,6 +1,10 @@
 #!/usr/bin/env groovy
 
 def call() {
-  echo "${wget https://ftp.acciaccatura.dk/static/media/toolkit/trivy-secret.yaml}"
-  echo "${trivy filesystem ./}"
+  sh "wget https://ftp.acciaccatura.dk/static/media/toolkit/trivy-secret.yaml"
+  BUILD_FULL = sh (
+      script: "trivy filesystem ./",
+      returnStatus: true
+  ) == 0
+  echo "Trivy results: ${BUILD_FULL}"
 }
